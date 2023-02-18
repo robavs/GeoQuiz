8f86455756d6a278e57350920df1884f2ec93a60
Aleksa Robavs <62261768+robavs@users.noreply.github.com>
2023-02-18 11:56:24 +0100

    Update continent.js

--- rest files/COUNTRIES/continent.js
+++ rest files/COUNTRIES/continent.js
@@ -1,7 +1,9 @@
-let europe = { 'Albanija': 'Tirana', 'Andora': 'Andora la Velja', 'Austrija': 'Beč', 'Belorusija': 'Minsk', 'Belgija': 'Brisel', 'Bosna i Hercegovina': 'Sarajevo', 'Bugarska': 'Sofija', 'Hrvatska': 'Zagreb', 'Češka': 'Prag', 'Kipar': 'Nikozija', 'Danska': 'Kopenhagen', 'Estonija': 'Talin', 'Finska': 'Helsinki', 'Francuska': 'Pariz', 'Nemačka': 'Berlin', 'Grčka': 'Atina', 'Mađarska': 'Budimpešta', 'Island': 'Rejkjavik', 'Irska': 'Dablin', 'Italija': 'Rim', 'Letonija': 'Riga', 'Lihtenštajn': 'Vaduz', 'Litvanija': 'Vilinjus', 'Luksemburg': 'Luksemburg', 'Makedonija': 'Skoplje', 'Malta': 'La Valeta', 'Moldavija': 'Kišinjev', 'Monako': 'Monako', 'Crna Gora': 'Podgorica', 'Holandija': 'Amsterdam', 'Norveška': 'Oslo', 'Poljska': 'Varšava', 'Portugalija': 'Lisabon', 'Rumunija': 'Bukurešt', 'Rusija': 'Moskva', 'San Marino': 'San Marino', 'Slovačka': 'Bratislava', 'Slovenija': 'Ljubljana', 'Španija': 'Madrid', 'Švedska': 'Stokholm', 'Švajcarska': 'Bern', 'Ukrajina': 'Kijev', 'Ujedinjeno Kraljevstvo':'London','Vatikan': 'Vatikan', 'Srbija': 'Beograd'},
-    africa = { 'Alžir': 'Alžir', 'Angola': 'Luanda', 'Benin': 'Porto Novo', 'Bocvana': 'Gaborone', 'Burkina Faso': 'Uagadugu', 'Borundi': 'Budžumbura', 'Gabon': 'Librvil', 'Gambija': 'Bandzul', 'Gana': 'Akra', 'Gvineja': 'Konakri', 'Gvineja Bisao': 'Bisau', 'Egipat': 'Kairo', 'Ekvatorijalna Gvineja': 'Malabo', 'Eritreja': 'Asmara', 'Etiopija': 'Adis Abeba', 'Zambija': 'Lusaka', 'Zelenortska Ostrva': 'Praja', 'Zimbabve': 'Harare', 'Južnoafrička Republika': 'Pretorija', 'Južni Sudan': 'Džuba', 'Kamerun': 'Jaunde', 'Kenija': 'Najrobi', 'Kongo': 'Brazavil', 'DR Kongo': 'Kinšasa', 'Komori': 'Moroni', 'Lesoto': 'Maseru', 'Liberija': 'Monrovija', 'Libija': 'Tripoli', 'Madagaskar': 'Antananarivo', 'Malavi': 'Lilongve', 'Mali': 'Bamako', 'Maroko': 'Rabat', 'Mauritanija': 'Nuakšot', 'Mauricijus': 'Port Luis', 'Mozambik': 'Maputo', 'Namibija': 'Vindhuk', 'Nigerija': 'Abudža', 'Niger': 'Nijamej', 'Obala Slonovače': 'Jamasukro', 'Ruanda': 'Kigali', 'Sijera Leone': 'Fritaun', 'Senegal': 'Dakar', 'Somalija': 'Mogadišu', 'Sudan': 'Kartun', 'Svazilend': 'Mbabane', 'Tanzanija': 'Dodoma', 'Togo': 'Lome', 'Tunis': 'Tunis', 'Uganda': 'Kampala', 'Centralnoafrička Republika': 'Bangi', 'Čad': 'Ndžamena', 'Džibuti': 'Džibuti', 'Maroko': 'Rabat','Sao Tome i Prinsipe':'Sao Tome' },
-    asia = { 'Avganistan': 'Kabul', 'Azerbejdžan': 'Baku', 'Bangladeš': 'Daka', 'Bahrein': 'Manama', 'Butan': 'Timbu',
-             'Bruneji':'Bandar Seri Begavan', 'Vijetnam': 'Hanoj', 'Gruzija': 'Tbilisi', 'Izrael': 'Jerusalim', 'Indija': 'Nju Delhi', 'Indonezija': 'Džakarta', 'Irak': 'Bagdad', 'Iran': 'Teheran', 'Istočni Timor': 'Dili', 'Japan': 'Tokio', 'Jemen': 'Sana', 'Jermenija': 'Jerevan', 'Jordan': 'Aman', 'Južna Koreja': 'Seul', 'Kazahstan': 'Astana', 'Kambodža': 'Pnom Pen', 'Katar': 'Doha', 'Kina': 'Peking', 'Kipar': 'Nikozija', 'Kirgistan': 'Biškek', 'Kuvajt': 'Kuvajt', 'Laos': 'Vijentijan', 'Liban': 'Bejrut', 'Maldivi': 'Male', 'Melezija': 'Kuala Lumpur', 'Mjanmar': 'Nejpjido', 'Mongolija': 'Ulan Bator', 'Nepal': 'Katmandu', 'Pakistan': 'Islamabad', 'Oman': 'Muskat', 'Rusija': 'Moskva', 'Saudijska Arabija': 'Rijad', 'Severna Koreja': 'Pjongjang', 'Singapur': 'Singapur', 'Sirija': 'Damask', 'Tajland': 'Bangkok', 'Tadžikistan': 'Dušanbe', 'Turkmenistan': 'Ashabad', 'Turska': 'Ankara', 'Uzbekistan': 'Taškent', 'Filipini': 'Manila', 'Ujedinjeni Arapski Emirati': 'Abu Dabi','Šri Lanka':'Kolombo','Tajvan':'Tajpej' },
+let europe = { 'Albanija': 'Tirana', 'Andora': 'Andora la Velja', 'Austrija': 'Beč', 'Belorusija': 'Minsk', 'Belgija': 'Brisel', 'Bosna i Hercegovina': 'Sarajevo', 'Bugarska': 'Sofija', 'Hrvatska': 'Zagreb', 'Češka': 'Prag', 'Kipar': 'Nikozija', 'Danska': 'Kopenhagen', 'Estonija': 'Talin', 'Finska': 'Helsinki', 'Francuska': 'Pariz', 'Nemačka': 'Berlin', 'Grčka': 'Atina', 'Mađarska': 'Budimpešta', 'Island': 'Rejkjavik', 'Irska': 'Dablin', 'Italija': 'Rim', 'Letonija': 'Riga', 'Lihtenštajn': 'Vaduz', 'Litvanija': 'Vilinjus', 'Luksemburg': 'Luksemburg', 'Makedonija': 'Skoplje', 'Malta': 'La Valeta', 'Moldavija': 'Kišinjev', 'Monako': 'Monako', 'Crna Gora': 'Podgorica', 'Holandija': 'Amsterdam', 'Norveška': 'Oslo', 'Poljska': 'Varšava', 'Portugalija': 'Lisabon', 'Rumunija': 'Bukurešt', 'Rusija': 'Moskva', 'San Marino': 'San Marino', 'Slovačka': 'Bratislava', 'Slovenija': 'Ljubljana', 'Španija': 'Madrid', 'Švedska': 'Stokholm', 'Švajcarska': 'Bern', 'Ukrajina': 'Kijev', 'Ujedinjeno Kraljevstvo': 'London', 'Vatikan': 'Vatikan', 'Srbija': 'Beograd' },
+    africa = { 'Alžir': 'Alžir', 'Angola': 'Luanda', 'Benin': 'Porto Novo', 'Bocvana': 'Gaborone', 'Burkina Faso': 'Uagadugu', 'Borundi': 'Budžumbura', 'Gabon': 'Librvil', 'Gambija': 'Bandzul', 'Gana': 'Akra', 'Gvineja': 'Konakri', 'Gvineja Bisao': 'Bisau', 'Egipat': 'Kairo', 'Ekvatorijalna Gvineja': 'Malabo', 'Eritreja': 'Asmara', 'Etiopija': 'Adis Abeba', 'Zambija': 'Lusaka', 'Zelenortska Ostrva': 'Praja', 'Zimbabve': 'Harare', 'Južnoafrička Republika': 'Pretorija', 'Južni Sudan': 'Džuba', 'Kamerun': 'Jaunde', 'Kenija': 'Najrobi', 'Kongo': 'Brazavil', 'DR Kongo': 'Kinšasa', 'Komori': 'Moroni', 'Lesoto': 'Maseru', 'Liberija': 'Monrovija', 'Libija': 'Tripoli', 'Madagaskar': 'Antananarivo', 'Malavi': 'Lilongve', 'Mali': 'Bamako', 'Maroko': 'Rabat', 'Mauritanija': 'Nuakšot', 'Mauricijus': 'Port Luis', 'Mozambik': 'Maputo', 'Namibija': 'Vindhuk', 'Nigerija': 'Abudža', 'Niger': 'Nijamej', 'Obala Slonovače': 'Jamasukro', 'Ruanda': 'Kigali', 'Sijera Leone': 'Fritaun', 'Senegal': 'Dakar', 'Somalija': 'Mogadišu', 'Sudan': 'Kartun', 'Svazilend': 'Mbabane', 'Tanzanija': 'Dodoma', 'Togo': 'Lome', 'Tunis': 'Tunis', 'Uganda': 'Kampala', 'Centralnoafrička Republika': 'Bangi', 'Čad': 'Ndžamena', 'Džibuti': 'Džibuti', 'Maroko': 'Rabat', 'Sao Tome i Prinsipe': 'Sao Tome' },
+    asia = {
+        'Avganistan': 'Kabul', 'Azerbejdžan': 'Baku', 'Bangladeš': 'Daka', 'Bahrein': 'Manama', 'Butan': 'Timbu',
+        'Bruneji': 'Bandar Seri Begavan', 'Vijetnam': 'Hanoj', 'Gruzija': 'Tbilisi', 'Izrael': 'Jerusalim', 'Indija': 'Nju Delhi', 'Indonezija': 'Džakarta', 'Irak': 'Bagdad', 'Iran': 'Teheran', 'Istočni Timor': 'Dili', 'Japan': 'Tokio', 'Jemen': 'Sana', 'Jermenija': 'Jerevan', 'Jordan': 'Aman', 'Južna Koreja': 'Seul', 'Kazahstan': 'Astana', 'Kambodža': 'Pnom Pen', 'Katar': 'Doha', 'Kina': 'Peking', 'Kipar': 'Nikozija', 'Kirgistan': 'Biškek', 'Kuvajt': 'Kuvajt', 'Laos': 'Vijentijan', 'Liban': 'Bejrut', 'Maldivi': 'Male', 'Melezija': 'Kuala Lumpur', 'Mjanmar': 'Nejpjido', 'Mongolija': 'Ulan Bator', 'Nepal': 'Katmandu', 'Pakistan': 'Islamabad', 'Oman': 'Muskat', 'Rusija': 'Moskva', 'Saudijska Arabija': 'Rijad', 'Severna Koreja': 'Pjongjang', 'Singapur': 'Singapur', 'Sirija': 'Damask', 'Tajland': 'Bangkok', 'Tadžikistan': 'Dušanbe', 'Turkmenistan': 'Ashabad', 'Turska': 'Ankara', 'Uzbekistan': 'Taškent', 'Filipini': 'Manila', 'Ujedinjeni Arapski Emirati': 'Abu Dabi', 'Šri Lanka': 'Kolombo', 'Tajvan': 'Tajpej'
+    },
     north_america = { 'Antigva i Barbuda': 'Sent Džons', 'Barbados': 'Bridžtaun', 'Bahami': 'Nasau', 'Belize': 'Belmopan', 'Gvatemala': 'Gvatemala', 'Grenada': 'Sent Džordžes', 'Dominika': 'Rozo', 'Dominikanska Republika': 'Santo Domingo', 'Salvador': 'San Salvador', 'Jamajka': 'Kingston', 'Kanada': 'Otava', 'Kostarika': 'San Hose', 'Kuba': 'Havana', 'Meksiko': 'Meksiko Siti', 'Nikaragva': 'Managva', 'Panama': 'Panama', 'Sveta Lucija': 'Kastri', 'Sveti Vinsent i Grenadini': 'Kingstaun', 'Sveti Kristofer i Nevis': 'Baster', 'Sjedinjene Američke Države': 'Vašington', 'Trindad i Tobago': 'Port of Spejn', 'Haiti': 'Port o Prens', 'Honduras': 'Tegusigalpa' },
     south_america = { 'Argentina': 'Buenos Ajres', 'Bolivija': 'Sukre', 'Brazil': 'Brazilija', 'Čile': 'Santijago', 'Kolumbija': 'Bogota', 'Ekvador': 'Kito', 'Gvajana': 'Džordžtaun', 'Paragvaj': 'Asunsion', 'Peru': 'Lima', 'Surinam': 'Paramaribo', 'Urugvaj': 'Montevideo', 'Venecuela': 'Karakas' },
     australia = { 'Australija': 'Kanbera', 'Fidži': 'Suva', 'Kiribati': 'Bairiki', 'Maršalska Ostrva': 'Madžuro', 'Mikronezija': 'Palikir', 'Nauru': 'Jaren', 'Novi Zeland': 'Velington', 'Palau': 'Ngerulmud', 'Papua Nova Gvineja': 'Port Morsbi', 'Samoa': 'Apija', 'Solomonova Ostrva': 'Honijara', 'Tonga': "Nukualofa", 'Tuvalu': 'Funafuti', 'Vanuatu': 'Port Vila' },
@@ -10,128 +12,140 @@ let europe = { 'Albanija': 'Tirana', 'Andora': 'Andora la Velja', 'Austrija': 'B
     your_score = document.getElementById('your_score'),
     total_score = Number(document.getElementById('number_of_countries').innerHTML.slice(2)),
     input = document.querySelector('input'),
-    paths =[...document.querySelectorAll('path')]
-    paths_name = paths.map(el=>{
-        if(el.classList[1].includes('_')){
-            return el.classList[1].replace(/[_]/g,' ').toLowerCase()
-        }
-        return el.classList[1].toLowerCase()
-    })
+    paths = [...document.querySelectorAll('.state')]
 
-back_btn.onclick=function(){
+paths_name = paths.map(el => {
+    if (el.classList[1].includes('_')) {
+        return el.classList[1].replace(/[_]/g, ' ').toLowerCase()
+    }
+    return el.classList[1].toLowerCase()
+})
+
+
+back_btn.onclick = function () {
     window.history.back()
 }
 
 const full_page_name = window.location.pathname,
-      page_name = full_page_name.split('/').pop().split('.').shift()
-      all_countries = Object.keys(eval(page_name)).map(el=>el.toLowerCase())
+    page_name = full_page_name.split('/').pop().split('.').shift()
+all_countries = Object.keys(eval(page_name)).map(el => el.toLowerCase())
 
-input.addEventListener('input',function(){
-    if(all_countries.includes(this.value.toLowerCase())){
+const stack = []
+
+input.addEventListener('input', function () {
+    if (all_countries.includes(this.value.toLowerCase())) {
         let index = all_countries.indexOf(this.value.toLowerCase())
-        all_countries.splice(index,1)
+        all_countries.splice(index, 1)
         your_score.innerHTML = ++score
-        if(paths_name.includes(this.value.toLowerCase())){
+        if (paths_name.includes(this.value.toLowerCase())) {
             //some countries are separated
             let first_index = paths_name.indexOf(this.value.toLowerCase())
             let second_index = paths_name.lastIndexOf(this.value.toLowerCase())
 
-            paths[first_index].style.fill='red'
-            paths[second_index].style.fill='red'
+
+            while (stack.length != 0) {
+                const country = stack.pop()
+                country.style.fill = "#50c878"
+            }
+
+            paths[first_index].style.fill = '#048dc4'
+            paths[second_index].style.fill = '#048dc4'
+
+            stack.push(paths[first_index], paths[second_index])
         }
-        input.value=''
+        input.value = ''
     }
 })
 
 let time_box = document.createElement('div'),
-    time_in_seconds = page_name=='europe' || page_name=='asia' || page_name=='africa' ? 300 : 120,
+    time_in_seconds = page_name == 'europe' || page_name == 'asia' || page_name == 'africa' ? 300 : 120,
     started_time = time_in_seconds
-time_box.innerHTML = `Your time is ${time_in_seconds/60}:00`
+time_box.innerHTML = `Your time is ${time_in_seconds / 60}:00`
 document.body.append(time_box)
 time_box.classList.add('time_box')
 
 //take last 4 because these are for scores
-const score_divs=[...document.querySelector('#last_scores').children].slice(1)
+const score_divs = [...document.querySelector('#last_scores').children].slice(1)
 let time_obj = {},
     score_obj = {}
 
-if(localStorage.getItem(`${page_name}_time`)){
+if (localStorage.getItem(`${page_name}_time`)) {
     //there we use reverse because we want to use latest results when game starts and to display that after each 1 second
     time_obj[page_name] = JSON.parse(localStorage.getItem(`${page_name}_time`)).reverse()
     score_obj[page_name] = JSON.parse(localStorage.getItem(`${page_name}_score`)).reverse()
 }
-else{
+else {
     time_obj[page_name] = []
     score_obj[page_name] = []
 }
 
 const interval = setInterval(() => {
     time_in_seconds--
-    let minutes = Math.floor(time_in_seconds/60)
-    let seconds = time_in_seconds - minutes*60
-    if(seconds<10){
-        time_box.innerHTML=`Your time is ${minutes}:0${seconds}`
+    let minutes = Math.floor(time_in_seconds / 60)
+    let seconds = time_in_seconds - minutes * 60
+    if (seconds < 10) {
+        time_box.innerHTML = `Your time is ${minutes}:0${seconds}`
     }
-    else{
-        time_box.innerHTML=`Your time is ${minutes}:${seconds}`
+    else {
+        time_box.innerHTML = `Your time is ${minutes}:${seconds}`
     }
-    if(time_in_seconds==0 || score == total_score){
+    if (time_in_seconds == 0 || score == total_score) {
         //create refresh button
         const refresh_btn = document.createElement('div')
-        refresh_btn.innerHTML='Restart'
+        refresh_btn.innerHTML = 'Restart'
         refresh_btn.classList.add('refresh_btn')
         document.body.append(refresh_btn)
-        refresh_btn.onclick=function(){
+        refresh_btn.onclick = function () {
             location.reload()
         }
         clearInterval(interval)
         let your_time = score == total_score ? started_time - time_in_seconds : started_time
         //storage time and score 
-        if(localStorage.getItem(`${page_name}_time`)){
+        if (localStorage.getItem(`${page_name}_time`)) {
             time_obj[page_name] = JSON.parse(localStorage.getItem(`${page_name}_time`))
             score_obj[page_name] = JSON.parse(localStorage.getItem(`${page_name}_score`))
         }
 
         time_obj[page_name].push(your_time)
         score_obj[page_name].push(score)
-        localStorage.setItem(`${page_name}_time`,JSON.stringify(time_obj[page_name]))
-        localStorage.setItem(`${page_name}_score`,JSON.stringify(score_obj[page_name]))
+        localStorage.setItem(`${page_name}_time`, JSON.stringify(time_obj[page_name]))
+        localStorage.setItem(`${page_name}_score`, JSON.stringify(score_obj[page_name]))
 
-        input.readOnly=true
+        input.readOnly = true
         //because we need latest results when game is finished
         score_obj[page_name].reverse()
         time_obj[page_name].reverse()
     }
-    for (let a in score_obj[page_name]){
-        if(a>3) break
-        if(score_obj[page_name].length>4){
-            score_obj[page_name].slice(0,4) 
+    for (let a in score_obj[page_name]) {
+        if (a > 3) break
+        if (score_obj[page_name].length > 4) {
+            score_obj[page_name].slice(0, 4)
         }
-        score_divs[a].innerHTML=''
-        score_divs[a].innerHTML+=`  ${score_obj[page_name][a]} pt.  →  ${time_obj[page_name][a]} sec.`
+        score_divs[a].innerHTML = ''
+        score_divs[a].innerHTML += `  ${score_obj[page_name][a]} pt.  →  ${time_obj[page_name][a]} sec.`
     }
 }, 1000)
 //listen for change orientation from landscape toportrait 
-window.addEventListener('orientationchange',()=>{
-    document.documentElement.addEventListener('click',function(e){
-        if(window.innerWidth < 650 && window.orientation == 0){
-            if(e.target.id == 'search_bar'){
-                document.querySelector('svg').style.display='none'
+window.addEventListener('orientationchange', () => {
+    document.documentElement.addEventListener('click', function (e) {
+        if (window.innerWidth < 650 && window.orientation == 0) {
+            if (e.target.id == 'search_bar') {
+                document.querySelector('svg').style.display = 'none'
             }
-            else{
-                document.querySelector('svg').style.display=''
+            else {
+                document.querySelector('svg').style.display = ''
             }
         }
     })
 })
 //display none svg in portrait mode
-document.documentElement.addEventListener('click',function(e){
-    if(window.innerWidth < 650){
-        if(e.target.id == 'search_bar'){
-            document.querySelector('svg').style.display='none'
+document.documentElement.addEventListener('click', function (e) {
+    if (window.innerWidth < 650) {
+        if (e.target.id == 'search_bar') {
+            document.querySelector('svg').style.display = 'none'
         }
-        else{
-            document.querySelector('svg').style.display=''
+        else {
+            document.querySelector('svg').style.display = ''
         }
     }
 })
